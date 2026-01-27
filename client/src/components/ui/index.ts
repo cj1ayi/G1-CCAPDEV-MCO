d6ce1a798095d3d297b416fe3ca2367840b3cf35
Floranaras <carlosranara0@gmail.com>
2026-01-27 17:07:29 +0800

    feat: add barrel exports for cleaner imports

---
+++ client/src/components/ui/index.ts
@@ -0,0 +1,36 @@
+export { Button } from './Button'
+export { Input } from './Input'
+export { Textarea } from './Textarea'
+export { Select } from './Select'
+export { Badge } from './Badge'
+export { Avatar } from './Avatar'
+
+export { 
+  Card, 
+  CardHeader, 
+  CardTitle, 
+  CardDescription, 
+  CardContent, 
+  CardFooter 
+} 
+from './Card'
+
+export { 
+  Modal, 
+  ModalHeader, 
+  ModalTitle, 
+  ModalContent, 
+  ModalFooter 
+} 
+
+from './Modal'
+
+
+export { 
+  Dropdown, 
+  DropdownItem, 
+  DropdownSeparator, 
+  DropdownLabel 
+} 
+
+from './Dropdown'
---
+++ client/src/pages/index.ts
@@ -0,0 +1,8 @@
+export { default as Home } from './Home'
+export { default as PostDetail } from './PostDetail'
+export { default as Profile } from './Profile'
+export { default as Space } from './Space'
+export { default as Login } from './Login'
+export { default as Signup } from './Signup'
+export { default as Search } from './Search'
+export { default as ComponentShowcase } from './ComponentShowcase'
