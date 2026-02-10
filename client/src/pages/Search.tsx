62246769bd5abe6aa101bfc5b7d28bacca680e1a
Teeheene <diane.panganiban1010@gmail.com>
2026-02-10 19:33:50 +0800

    chore: add sidebar nav to search

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -1,15 +1,34 @@
 import { MainLayout } from '@/components/layout/MainLayout'
 import { SearchResults } from '@/features/search/components/SearchResults'
+import { TrendingWidgets } from '@/features/search/components/TrendingWidgets'
+import { SidebarNav } from '@/features/navigation/components/SidebarNav'
 
 const Search = () => {
   return (
-		<MainLayout
-			headerVariant="search"
-			maxWidth="max-w-7xl"
-		>
-			<SearchResults />
-		</MainLayout>
+    <MainLayout
+      headerVariant="search"
+      maxWidth="max-w-7xl"
+    >
+      <div className="grid grid-cols-12 gap-6">
+        
+        {/* Left */}
+        <aside className="col-span-3 space-y-4">
+					<SidebarNav />
+        </aside>
+
+        {/* Center */}
+        <main className="col-span-6 space-y-4">
+          <SearchResults />
+        </main>
+
+        {/* Right */}
+        <aside className="col-span-3 space-y-4">
+          <TrendingWidgets />
+        </aside>
+      </div>
+    </MainLayout>
   )
 }
+
 
 export default Search
