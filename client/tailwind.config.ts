821b959f273e45761fb9c2d5339ebf537f905277
Floranaras <carlosranara0@gmail.com>
2026-02-20 16:04:54 +0800

    refactor: changed the darkmode hierarchy

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -12,18 +12,18 @@ export default {
         primary: '#007036',
         'primary-dark': '#005c2d',
         'primary-light': '#00944a',
-        
+
         // Light mode
         'background-light': '#f2f4f3',
         'surface-light': '#ffffff',
         'border-light': '#dae7e0',
-        
-        'background-dark': '#030303',
-        'surface-dark': '#1a1a1b',
-        'surface-darker': '#0d0d0e',
-        'surface-input': '#272729',
-        'border-dark': '#343536',         
-        
+
+        // Dark mode - better hierarchy
+        'background-dark': '#0a0e11', 
+        'surface-dark': '#1a1a1b',         
+        'surface-darker': '#272729',       
+        'surface-input': '#3a3a3c',        
+        'border-dark': '#404040',          
         'dlsu-gold': '#C1A624',
       },
       fontFamily: {
