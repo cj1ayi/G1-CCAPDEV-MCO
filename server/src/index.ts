c28c251901b135236ff45296955021d9dbb15294
yaraikun <evan.calupig@gmail.com>
2026-03-01 22:29:51 +0800

    chore: set up configs for backend

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('AnimoSpaces API is running...');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
