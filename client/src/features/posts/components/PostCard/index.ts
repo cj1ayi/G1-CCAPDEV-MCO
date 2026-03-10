87d855febd87f068a5eafe22c0da5e57ccb46cbb
Floranaras <carlosranara0@gmail.com>
2026-03-10 08:27:35 +0800

    chore: remove PostCardActions export from index

--- client/src/features/posts/components/PostCard/index.ts
+++ client/src/features/posts/components/PostCard/index.ts
@@ -2,7 +2,6 @@ export { default as PostCard } from './PostCard'
 export { PostCardVoting } from './PostCardVoting'
 export { PostCardHeader } from './PostCardHeader'
 export { PostCardContent } from './PostCardContent'
-export { PostCardActions } from './PostCardActions'
 
 export type {
   PostCardVotingProps,
